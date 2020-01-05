d8fa9fedee6678d4fa43396c1b79813086579e45
vladimirventaniuc <vladimir.ventaniuc@yahoo.com>
2020-01-05 17:07:49 +0200

    xml parser

---
+++ bda/Main.py
@@ -0,0 +1,148 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+import xml.etree.ElementTree as ET
+import re
+import os
+
+class Article(object):
+  article_type = ''
+  journal_title = ''
+  article_categories = []
+  article_title = ''
+  authors = []
+  chapters = []
+
+  def __init__(self, article_type, journal_title, article_title, authors, article_categories, chapters):
+    self.article_type = article_type
+    self.journal_title = journal_title
+    self.article_categories = article_categories
+    self.article_title=article_title
+    self.authors=authors
+    self.chapters = chapters
+
+def init_spark():
+  spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
+  sc = spark.sparkContext
+  return spark,sc
+
+def read_record(xmlns, record):
+  article = record.find(xmlns + 'metadata')[0]
+  article_type = article.attrib['article-type']
+  journal_title = ''
+  article_categories = []
+  article_title = ''
+  authors = []
+  chapters = []
+
+  m = re.search('{.*}', article.tag)
+  article_xmlns = m.group(0)
+
+  front = article.find(article_xmlns + 'front')
+  body = article.find(article_xmlns + 'body')
+
+  journal_title = front.find(article_xmlns+'journal-meta/'+article_xmlns+'journal-title-group/'+article_xmlns+'journal-title').text
+
+  article_meta = front.find(article_xmlns+'article-meta')
+
+  article_categories_el = article_meta.find(article_xmlns+'article-categories')
+  for el in article_categories_el:
+    article_categories.append(el[0].text)
+
+  article_title = article_meta.find(article_xmlns+'title-group/'+article_xmlns+'article-title').text
+  contrib_group = article_meta.find(article_xmlns + 'contrib-group')
+
+  if contrib_group is not None:
+    for contrib in contrib_group:
+      first_name = contrib.find(article_xmlns+'name/'+article_xmlns+'given-names')
+      last_name = contrib.find(article_xmlns+'name/'+article_xmlns+'surname')
+      if first_name != None and last_name != None:
+        authors.append((first_name.text, last_name.text))
+
+  body_content = body.findall(article_xmlns+'sec')
+  for sec in body_content:
+    title, content = extract_title_and_body(sec,article_xmlns)
+    chapters.append((title, content))
+
+  return Article(article_type, journal_title, article_title, authors, article_categories, chapters)
+
+def extract_title_and_body(chapter, article_xmlns):
+  title_tag = chapter.find(article_xmlns + 'title')
+  title = ''
+  if title_tag is not None:
+    title = title_tag.text
+
+  paragraphs = chapter.findall(article_xmlns + 'p')
+  content = ''
+  for p in paragraphs:
+    if p.text != None:
+      content = content + p.text
+  inner_secvences = chapter.findall(article_xmlns + 'sec')
+  for sec in inner_secvences:
+    inner_title, inner_content = extract_title_and_body(sec, article_xmlns)
+    content = content + inner_content
+  return title, content
+
+def read_xml(path):
+  tree = ET.parse(path)
+  root = tree.getroot()
+
+  m = re.search('{.*}', root.tag)
+  xmlns = m.group(0)
+
+  records = root.findall(xmlns + "ListRecords/" + xmlns + "record")
+
+  articles = []
+  for record in records:
+    articles.append(read_record(xmlns, record))
+  return articles
+
+def create_xml_file(articles):
+  data = ET.Element('data')
+  for article in articles:
+    article_tag = ET.SubElement(data, 'article')
+    title_tag = ET.SubElement(article_tag,'title')
+    title_tag.text = article.article_title
+    type_tag = ET.SubElement(article_tag,'article-type')
+    type_tag.text = article.article_type
+    journal_title_tag = ET.SubElement(article_tag, 'journal-title')
+    journal_title_tag.text = article.journal_title
+    authors_tag = ET.SubElement(article_tag,'authors')
+    for author in article.authors:
+      author_tag = ET.SubElement(authors_tag,'author')
+      first_name = ET.SubElement(author_tag,'first-name')
+      last_name = ET.SubElement(author_tag, 'last-name')
+      first_name.text = author[0]
+      last_name.text = author[1]
+
+    article_categories_tag = ET.SubElement(article_tag,'article-categories')
+    for category in article.article_categories:
+      category_tag = ET.SubElement(article_categories_tag, 'category')
+      category_tag.text = category
+
+    chapters_tag = ET.SubElement(article_tag,'chapters')
+    for chapter in article.chapters:
+      chapter_tag = ET.SubElement(chapters_tag, 'chapter')
+      if chapter[0] is not None:
+        chapter_tag.set('title',chapter[0])
+      else:
+        chapter_tag.set('title', 'None')
+      chapter_tag.text = chapter[1]
+  # create a new XML file with the results
+  tree = ET.ElementTree(data)
+  tree.write("./result/articles.xml")
+
+def main():
+  spark,sc = init_spark()
+  files = []
+  articles = []
+  for r, d, f in os.walk('./xml/'):
+    for file in f:
+      if '.xml' in file:
+        files.append(os.path.join(r, file))
+
+  for file in files:
+    articles.extend(read_xml(file))
+  create_xml_file(articles)
+
+if __name__ == '__main__':
+  main()
